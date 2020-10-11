192eb01296fed7eb967f367d1a9d10f4fb77fc98
Raufeder <you@example.com>
2020-10-11 17:28:06 -0400

    10/11

--- scripts.js
+++ scripts.js
@@ -1,28 +1,39 @@
 const baseURL = 'https://pokeapi.co/api/v2/';
 const pokemon = 'pokemon/';
-const maxOptions = '?=150'
 const randomizer = document.querySelector('.random');
-const number = document.querySelector('.numbers');
-let url;
+const pokeName = document.getElementById('name');
+const number = Math.floor(Math.random() * Math.floor(894));
 
 randomizer.addEventListener('click', fetchPoke);
-number.addEventListener('click', randomNum);
 
 function fetchPoke(e) {
     e.preventDefault(); 
 
-    fetch(baseURL + pokemon) 
+    fetch(baseURL + pokemon + number) 
         .then(results => {
             return results.json() 
         })
-        .then(json => [
-            console.log(json)
-    ]);
+        .then(json => {
+            console.log(json);
+            displayResults(json);
+        });
 };
 
-function randomNum() {
-    let x = document.getElementById("numberGenerator");
-    x.innerHTML = Math.floor((Math.random() * 150) + 1);
-    return x
+function displayResults(json) {
+    let nameData = json.name;
+    let hpData = json.stats[0].base_stat;
+    let attackData = json.stats[1].base_stat;
+    let defenseData = json.stats[2].base_stat;
+    let specialAttackData = json.stats[3].base_stat;
+    let specialDefenseData = json.stats[4].base_stat;
+    let speedData = json.stats[5].base_stat;
+
+    pokeName.innerHTML = nameData;
+    hp.innerHTML = hpData;
+    attack.innerHTML = attackData;
+    defense.innerHTML = defenseData;
+    specialAttack.innerHTML = specialAttackData;
+    specialDefense.innerHTML = specialDefenseData;
+    speed.innerHTML = speedData;
 };
 
